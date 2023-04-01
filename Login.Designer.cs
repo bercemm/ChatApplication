54f4972e1043e82bebee5ef59d8b70e24c60cd1e
berçem uçar <brcmmucr@hotmail.com>
2023-04-01 22:24:20 +0300

    giris düzenleme 2

---
+++ Login.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ChatApplication
+{
+    partial class Login
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.lblkullaniciadi = new System.Windows.Forms.Label();
+            this.lblsifre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtKullanici.Location = new System.Drawing.Point(56, 68);
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(198, 22);
+            this.txtKullanici.TabIndex = 0;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtSifre.Location = new System.Drawing.Point(56, 127);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(198, 22);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.Color.Violet;
+            this.btnGiris.ForeColor = System.Drawing.Color.Transparent;
+            this.btnGiris.Location = new System.Drawing.Point(113, 193);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(91, 27);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "Giriş Yap";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // lblkullaniciadi
+            // 
+            this.lblkullaniciadi.AutoSize = true;
+            this.lblkullaniciadi.ForeColor = System.Drawing.Color.Indigo;
+            this.lblkullaniciadi.Location = new System.Drawing.Point(53, 49);
+            this.lblkullaniciadi.Name = "lblkullaniciadi";
+            this.lblkullaniciadi.Size = new System.Drawing.Size(77, 16);
+            this.lblkullaniciadi.TabIndex = 3;
+            this.lblkullaniciadi.Text = "kullanıcı adı";
+            // 
+            // lblsifre
+            // 
+            this.lblsifre.AutoSize = true;
+            this.lblsifre.ForeColor = System.Drawing.Color.Indigo;
+            this.lblsifre.Location = new System.Drawing.Point(56, 105);
+            this.lblsifre.Name = "lblsifre";
+            this.lblsifre.Size = new System.Drawing.Size(32, 16);
+            this.lblsifre.TabIndex = 4;
+            this.lblsifre.Text = "sifre";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 343);
+            this.Controls.Add(this.lblsifre);
+            this.Controls.Add(this.lblkullaniciadi);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullanici);
+            this.ForeColor = System.Drawing.Color.DeepPink;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Login";
+            this.Text = "Giris Ekrani";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKullanici;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Label lblkullaniciadi;
+        private System.Windows.Forms.Label lblsifre;
+    }
+}
+
--- Login.cs
+++ Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ChatApplication
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
@@ -34,5 +34,6 @@ namespace ChatApplication
             }
       
         }
+
     }
 }
