74339b24ffb4a21e36df48a8ec01e0692cf1cebb
berçem uçar <brcmmucr@hotmail.com>
2023-04-21 01:00:31 +0300

    Firebasede kullanıcı tablosu oluşturuldu

---
+++ Tablolar/kullanici.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication.Tablolar // bu class firebase de bi tabloyu temsil eder
+{
+    public class kullanici //otomatik internal oluyor bunu public olarak değiştirmemiz gerekiyor ki her yerden erişilebilsin
+    {
+        //aşağıdaki özellikler kullanıcı tablosundaki sütunlara denk gelir.
+        public string email { get; set; }
+        public string sifre { get; set; }
+        public string tamad { get; set; }
+
+    }
+}
